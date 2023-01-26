e289505aad5fad3f8765f50d56fbd49e476ab76f
Debora Cocchi <108935821+DeboraCocchi@users.noreply.github.com>
2023-01-26 21:14:52 +0100

    [bonus-1]

---
+++ resources/js/App.vue
@@ -0,0 +1,37 @@
+<script>
+import axios from 'axios'
+export default {
+    name:'App',
+    data(){
+        return{
+        baseUrl:'http://127.0.0.1:8002/api/projects',
+        projects_collection:[]
+        }
+    },
+    methods:{
+        getProjects(){
+        axios.get(store.apiUrl)
+        .then(result =>{
+            console.log(result.data.projects.data);
+            this.projects_collection= result.data.projects.data;
+        })
+        .catch( err=>{
+            console.log('Si è verificato un errore');
+        })
+        },
+  },
+    mounted(){
+        this.getProjects();
+    }
+}
+</script>
+<template>
+
+
+
+</template>
+
+
+<style lang="scss">
+ @use '../scss/appVue.scss';
+</style>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,9 +13,12 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(['resources/css/appVue.scss', 'resources/js/appVue.js'])
     </head>
+
     <body class="font-sans text-gray-900 antialiased">
+
+        <div id="app">
         @include('partials.header')
         <main>
             @yield('content')
@@ -33,6 +36,6 @@
             </div>
         </div> --}}
 
-
+    </div>
     </body>
 </html>
