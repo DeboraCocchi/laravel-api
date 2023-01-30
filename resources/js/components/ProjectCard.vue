526c725c9ce5094c80c5d43a4a61a09f3e5c85a0
Debora Cocchi <108935821+DeboraCocchi@users.noreply.github.com>
2023-01-30 16:57:04 +0100

    [fix]function-date-bug

--- resources/js/components/ProjectCard.vue
+++ resources/js/components/ProjectCard.vue
@@ -16,15 +16,15 @@ export default {
 }
 </script>
 <template>
-<div class="dc-card my-3">
+<div class="dc-card my-3 p-0">
     <router-link :to="{name: 'project-detail', params:{ slug: project.slug} }">
         <div class="content-card h-100 w-100">
       <img :src="project.cover_image" class="card-img-top" :alt="project.image_original_name">
       <div class="card-body">
-        <h4 class="card-title mb-2 text-black">{{project.name}}</h4>
-        <h5 class="card-title mb-2">{{project.client_name}}</h5>
-        <p class="card-text text-black">{{this.cutText(project.summary)}}</p>
-        <div class="technologies p-3 m-0">
+        <h4 class="card-title mb-1 text-black">{{project.name}}</h4>
+        <h5 class="card-title mb-1">{{project.client_name}}</h5>
+        <p class="card-text text-black m-0">{{this.cutText(project.summary)}}</p>
+        <div class="technologies p-1 m-0">
           <span v-for="technology in project.technologies" :key="technology.id" class="badge">{{technology.name}}</span>
         </div>
       </div>
@@ -39,21 +39,17 @@ export default {
 <style lang="scss" scoped>
   .dc-card{
     width:18%;
-
     border-radius:0.8rem;
-    box-shadow: 3px 3px rgba(0,0,0,0.4);
+    box-shadow: 6px 8px 3px rgba(0, 0, 0, 0.187);
     .content-card{
-
       background-color:aliceblue;
       .card-body{
         text-align: center;
         padding:20px;
-
+        overflow-y:auto;
         h5.card-title{
              color:#9160f5;
         }
-
-
         .technologies{
           justify-content: center;
           width:100%;
@@ -73,8 +69,10 @@ export default {
     }
     img{
       width:100%;
-      height:50%;
+      max-height:200px;
       object-fit: cover;
+      border-top-left-radius:0.8rem;
+      border-top-right-radius:0.8rem;
     }
   }
 </style>
--- resources/js/pages/ProjDetail.vue
+++ resources/js/pages/ProjDetail.vue
@@ -1,38 +1,55 @@
-<script>
-import axios from 'axios'
-import {apiUrl} from '../data'
 
-export default {
-    name: 'About',
-    data(){
-        return{
-            apiUrl,
-            project: []
-        }
-    },
-    methods:{
-        getProjects(){
-            console.log(this.$route.params);
-        axios.get(apiUrl+this.$route.params.slug)
-        .then(result =>{
-            console.log(result.data);
-            this.project= result.data;
-        })
-        .catch( err=>{
-            console.log('Si è verificato un errore');
-        })
+<script>
+    import axios from 'axios'
+    import {apiUrl} from '../data'
+
+    export default {
+        name: 'About',
+        data(){
+            return{
+                apiUrl,
+                project: []
+            }
         },
-        reverseDate(date){
-            return date.split("-").reverse().join("/")
+        methods:{
+            getProjects(){
+                console.log(this.$route.params);
+            axios.get(apiUrl+this.$route.params.slug)
+            .then(result =>{
+                console.log(result.data);
+                this.project= result.data;
+            })
+            .catch( err=>{
+                console.log('Si è verificato un errore');
+            })
+            },
+            manipulateDate(date){
+                date=new Date()
+                let y=date.getFullYear()
+
+                let m=(date.getMonth())+1
+                if(m<10){
+                    m='0'+m
+                }
+                let d=date.getDate()
+                if(d<10){
+                    d='0'+d
+                }
+                return date=d+'/'+m+'/'+y
+            }
+        },
+        mounted(){
+            console.log(this.project);
+            this.getProjects()
+
+            }
         }
-    },
-    mounted(){
-        console.log(this.project);
-        this.getProjects()
-    }
-}
 
 </script>
+
+
+
+
 <template>
     <div class="container-fluid">
         <div class="d-flex justify-content-between my-4">
@@ -44,7 +61,7 @@ export default {
             <div class="col-3">
                 <img :src="project.cover_image" :alt="project.name">
             </div>
-            <div class="col-9">
+            <div class="col-8 ms-3">
                 <h2><span class="purple">Title: </span>{{project.name}}</h2>
                  <span v-if="project.type" class="badge text-bg-primary  d-inline-block mb-3">{{project.type.name}}</span>
 
@@ -52,7 +69,7 @@ export default {
 
 
 
-                <p><span class="purple">Created on: </span>{{reverseDate(project.created)}}</p>
+                <p><span class="purple">Created on: </span>{{manipulateDate(project.created)}}</p>
 
                 <p>
                     <span class="purple">Summary: </span>
@@ -72,6 +89,7 @@ export default {
 </template>
 
 
+
 <style lang="scss" scoped>
     .purple{
           color:#9160f5;
@@ -84,4 +102,10 @@ export default {
             text-decoration:underline;
     }
         }
+    img{
+        min-height: 400px;
+        width:auto;
+        max-width: 100%;
+        object-fit: cover;
+     }
 </style>
